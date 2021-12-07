69aef51b7e830bbe4560de1d7babd8576ff1693b
Jeff Lee <jeffomatic@gmail.com>
2021-12-06 21:52:04 -0800

    day07a: finish

---
+++ 2021-rust/day07a/src/main.rs
@@ -0,0 +1,30 @@
+use std::io::{self, Read};
+
+fn main() {
+    let positions: Vec<i32> = get_input()
+        .split(',')
+        .map(|str| str.parse().unwrap())
+        .collect();
+
+    let (min, max) = positions
+        .iter()
+        .fold((i32::MAX, i32::MIN), |(min, max), pos| {
+            (*pos.min(&min), *pos.max(&max))
+        });
+
+    let mut best_cost = i32::MAX;
+    for i in min..=max {
+        let cost = positions
+            .iter()
+            .fold(0, |accum, pos| accum + (pos - i).abs());
+        best_cost = best_cost.min(cost);
+    }
+
+    println!("{}", best_cost);
+}
+
+fn get_input() -> String {
+    let mut input = String::new();
+    io::stdin().lock().read_to_string(&mut input).unwrap();
+    return input.trim().to_string();
+}
