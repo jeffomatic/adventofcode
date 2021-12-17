32c75d55db6e3c7419368a0641afc14985e5a0ba
Jeff Lee <jeffomatic@gmail.com>
2021-12-16 16:36:20 -0800

    day16b: cleanup

--- 2021-rust/day16b/src/main.rs
+++ 2021-rust/day16b/src/main.rs
@@ -6,10 +6,21 @@ struct LiteralPacket {
     val: u64,
 }
 
+#[derive(Debug, Clone, Copy)]
+enum OpType {
+    Sum,
+    Product,
+    Min,
+    Max,
+    GreaterThan,
+    LessThan,
+    EqualTo,
+}
+
 #[derive(Debug, Clone)]
 struct OperatorPacket {
     version: u8,
-    type_id: u8,
+    optype: OpType,
     subpackets: Vec<Packet>,
 }
 
@@ -19,44 +30,47 @@ enum Packet {
     Operator(OperatorPacket),
 }
 
-fn read_version(bits: &mut dyn Iterator<Item = char>) -> u8 {
-    let version_bits: String = bits.take(3).collect();
-    return u8::from_str_radix(&version_bits, 2).unwrap();
+fn parse_version(bits: &mut dyn Iterator<Item = char>) -> u8 {
+    let data: String = bits.take(3).collect();
+    return u8::from_str_radix(&data, 2).unwrap();
 }
 
-fn read_type_id(bits: &mut dyn Iterator<Item = char>) -> u8 {
-    let version_bits: String = bits.take(3).collect();
-    return u8::from_str_radix(&version_bits, 2).unwrap();
+fn parse_type_id(bits: &mut dyn Iterator<Item = char>) -> u8 {
+    let data: String = bits.take(3).collect();
+    return u8::from_str_radix(&data, 2).unwrap();
 }
 
-fn read_literal(bits: &mut dyn Iterator<Item = char>) -> u64 {
-    let mut literal_bits = Vec::new();
+fn parse_literal(bits: &mut dyn Iterator<Item = char>) -> u64 {
+    let mut lit_bits = Vec::new();
     loop {
         let last = bits.next().unwrap() == '0';
-        literal_bits.extend(bits.take(4));
+        lit_bits.extend(bits.take(4));
         if last {
-            let bitstring = literal_bits.iter().collect::<String>();
+            let bitstring = lit_bits.iter().collect::<String>();
             return u64::from_str_radix(&bitstring, 2).unwrap();
         }
     }
 }
 
-fn read_operator_subpackets(bits: &mut dyn Iterator<Item = char>) -> Vec<Packet> {
+fn parse_binary_usize(bits: &mut dyn Iterator<Item = char>, nbits: usize) -> usize {
+    usize::from_str_radix(&bits.take(nbits).collect::<String>(), 2).unwrap()
+}
+
+fn parse_operator_subpackets(bits: &mut dyn Iterator<Item = char>) -> Vec<Packet> {
     let mut res = Vec::new();
 
     let length_type_id = bits.next().unwrap();
     match length_type_id {
         '0' => {
-            let nbits = usize::from_str_radix(&bits.take(15).collect::<String>(), 2).unwrap();
-            let mut subpacket_bits = bits.take(nbits).peekable();
-            while let Some(_) = subpacket_bits.peek() {
-                res.push(read_packet(&mut subpacket_bits));
+            let nbits = parse_binary_usize(bits, 15);
+            let mut sub_bits = bits.take(nbits).peekable();
+            while sub_bits.peek().is_some() {
+                res.push(parse_packet(&mut sub_bits));
             }
         }
         '1' => {
-            let nsubpackets = usize::from_str_radix(&bits.take(11).collect::<String>(), 2).unwrap();
-            for _ in 0..nsubpackets {
-                res.push(read_packet(bits));
+            for _ in 0..parse_binary_usize(bits, 11) {
+                res.push(parse_packet(bits));
             }
         }
         _ => unreachable!(),
@@ -65,19 +79,28 @@ fn read_operator_subpackets(bits: &mut dyn Iterator<Item = char>) -> Vec<Packet>
     return res;
 }
 
-fn read_packet(bits: &mut dyn Iterator<Item = char>) -> Packet {
-    let version = read_version(bits);
-    let type_id = read_type_id(bits);
+fn parse_packet(bits: &mut dyn Iterator<Item = char>) -> Packet {
+    let version = parse_version(bits);
+    let type_id = parse_type_id(bits);
 
     match type_id {
         4 => Packet::Literal(LiteralPacket {
             version,
-            val: read_literal(bits),
+            val: parse_literal(bits),
         }),
         _ => Packet::Operator(OperatorPacket {
             version,
-            type_id,
-            subpackets: read_operator_subpackets(bits),
+            optype: match type_id {
+                0 => OpType::Sum,
+                1 => OpType::Product,
+                2 => OpType::Min,
+                3 => OpType::Max,
+                5 => OpType::GreaterThan,
+                6 => OpType::LessThan,
+                7 => OpType::EqualTo,
+                _ => unimplemented!(),
+            },
+            subpackets: parse_operator_subpackets(bits),
         }),
     }
 }
@@ -87,33 +110,32 @@ fn eval(p: &Packet) -> u64 {
         Packet::Literal(lit) => lit.val,
         Packet::Operator(op) => {
             let mut sub_vals = op.subpackets.iter().map(|sub| eval(sub));
-            match op.type_id {
-                0 => sub_vals.sum(),
-                1 => sub_vals.product(),
-                2 => sub_vals.min().unwrap(),
-                3 => sub_vals.max().unwrap(),
-                5 => {
+            match op.optype {
+                OpType::Sum => sub_vals.sum(),
+                OpType::Product => sub_vals.product(),
+                OpType::Min => sub_vals.min().unwrap(),
+                OpType::Max => sub_vals.max().unwrap(),
+                OpType::GreaterThan => {
                     if sub_vals.next().unwrap() > sub_vals.next().unwrap() {
                         1
                     } else {
                         0
                     }
                 }
-                6 => {
+                OpType::LessThan => {
                     if sub_vals.next().unwrap() < sub_vals.next().unwrap() {
                         1
                     } else {
                         0
                     }
                 }
-                7 => {
+                OpType::EqualTo => {
                     if sub_vals.next().unwrap() == sub_vals.next().unwrap() {
                         1
                     } else {
                         0
                     }
                 }
-                _ => unreachable!(),
             }
         }
     }
@@ -134,7 +156,7 @@ fn main() {
         })
         .map(|v| if v { '1' } else { '0' });
 
-    println!("{}", eval(&read_packet(&mut bits)));
+    println!("{}", eval(&parse_packet(&mut bits)));
 }
 
 fn get_input() -> String {
