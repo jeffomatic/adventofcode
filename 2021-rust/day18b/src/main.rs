f9bea750d3fc2ebdf89452fc29ff6de49969c6bd
Jeff Lee <jeffomatic@gmail.com>
2021-12-18 15:39:55 -0800

    day18b: more cleanup

--- 2021-rust/day18b/src/main.rs
+++ 2021-rust/day18b/src/main.rs
@@ -7,142 +7,134 @@ struct VecTree {
 }
 
 impl VecTree {
-    fn new() -> VecTree {
-        VecTree {
+    fn parse(s: &str) -> VecTree {
+        let mut t = VecTree {
             vals: Vec::new(),
             depths: Vec::new(),
-        }
-    }
-}
-
-fn parse(s: &str) -> VecTree {
-    let mut t = VecTree::new();
-
-    let mut depth = 0;
-    for c in s.chars() {
-        match c {
-            '[' => {
-                depth += 1;
-            }
-            ',' => (),
-            ']' => {
-                depth -= 1;
-            }
-            d => {
-                t.vals.push(d.to_digit(10).unwrap());
-                t.depths.push(depth - 1);
-            }
-        }
-    }
-
-    t
-}
-
-fn explode(t: &mut VecTree) -> bool {
-    for i in 0..t.depths.len() {
-        let depth = t.depths[i];
-        if depth != 4 {
-            continue;
-        }
-
-        // add left value to left neighbor
-        if i != 0 {
-            t.vals[i - 1] += t.vals[i];
-        }
-
-        // add right value to right neighbor
-        if i + 2 < t.vals.len() {
-            t.vals[i + 2] += t.vals[i + 1];
-        }
-
-        t.vals[i] = 0;
-        t.depths[i] = 3;
-        t.vals.remove(i + 1);
-        t.depths.remove(i + 1);
-
-        return true;
-    }
-
-    false
-}
-
-fn split(t: &mut VecTree) -> bool {
-    for i in 0..t.vals.len() {
-        let v = t.vals[i];
-        if v < 10 {
-            continue;
-        }
-
-        let (a, b) = if v % 2 == 0 {
-            (v / 2, v / 2)
-        } else {
-            (v / 2, v / 2 + 1)
         };
 
-        t.vals[i] = a;
-        t.depths[i] += 1;
-        t.vals.insert(i + 1, b);
-        t.depths.insert(i + 1, t.depths[i]);
-
-        return true;
-    }
-
-    false
-}
-
-fn reduce(t: &mut VecTree) {
-    loop {
-        let did_explode = explode(t);
-        if did_explode {
-            continue;
-        }
-
-        let did_split = split(t);
-        if !did_explode && !did_split {
-            break;
-        }
-    }
-}
-
-fn add_array_tree(a: &mut VecTree, b: &mut VecTree) {
-    a.vals.append(&mut b.vals);
-    a.depths.append(&mut b.depths);
-    for i in 0..a.depths.len() {
-        a.depths[i] += 1;
-    }
-}
-
-fn score(t: &mut VecTree) -> u32 {
-    while t.vals.len() > 1 {
-        for i in 0..t.depths.len() - 1 {
-            if t.depths[i] == t.depths[i + 1] {
-                t.vals[i] = 3 * t.vals[i] + 2 * t.vals[i + 1];
-                t.vals.remove(i + 1);
-                t.depths.remove(i + 1);
-
-                if t.depths[i] > 0 {
-                    t.depths[i] -= 1;
+        let mut depth = 0;
+        for c in s.chars() {
+            match c {
+                '[' => {
+                    depth += 1;
                 }
+                ',' => (),
+                ']' => {
+                    depth -= 1;
+                }
+                d => {
+                    t.vals.push(d.to_digit(10).unwrap());
+                    t.depths.push(depth - 1);
+                }
+            }
+        }
 
+        t
+    }
+
+    fn explode(&mut self) -> bool {
+        for i in 0..self.depths.len() {
+            let depth = self.depths[i];
+            if depth != 4 {
+                continue;
+            }
+
+            // add left value to left neighbor
+            if i != 0 {
+                self.vals[i - 1] += self.vals[i];
+            }
+
+            // add right value to right neighbor
+            if i + 2 < self.vals.len() {
+                self.vals[i + 2] += self.vals[i + 1];
+            }
+
+            self.vals[i] = 0;
+            self.depths[i] = 3;
+            self.vals.remove(i + 1);
+            self.depths.remove(i + 1);
+
+            return true;
+        }
+
+        false
+    }
+
+    fn split(&mut self) -> bool {
+        for i in 0..self.vals.len() {
+            let v = self.vals[i];
+            if v < 10 {
+                continue;
+            }
+
+            let (a, b) = if v % 2 == 0 {
+                (v / 2, v / 2)
+            } else {
+                (v / 2, v / 2 + 1)
+            };
+
+            self.vals[i] = a;
+            self.depths[i] += 1;
+            self.vals.insert(i + 1, b);
+            self.depths.insert(i + 1, self.depths[i]);
+
+            return true;
+        }
+
+        false
+    }
+
+    fn reduce(&mut self) {
+        loop {
+            if !self.explode() && !self.split() {
                 break;
             }
         }
     }
 
-    t.vals[0]
+    fn add(&mut self, other: &VecTree) {
+        self.vals.extend(other.vals.iter());
+        self.depths.extend(other.depths.iter());
+        for i in 0..self.depths.len() {
+            self.depths[i] += 1;
+        }
+    }
+
+    fn score(&self) -> u32 {
+        let mut vals = self.vals.clone();
+        let mut depths = self.depths.clone();
+
+        while vals.len() > 1 {
+            for i in 0..depths.len() - 1 {
+                if depths[i] == depths[i + 1] {
+                    vals[i] = 3 * vals[i] + 2 * vals[i + 1];
+                    vals.remove(i + 1);
+                    depths.remove(i + 1);
+
+                    if depths[i] > 0 {
+                        depths[i] -= 1;
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        vals[0]
+    }
 }
 
 fn main() {
-    let trees: Vec<VecTree> = get_input().lines().map(parse).collect();
+    let trees: Vec<VecTree> = get_input().lines().map(VecTree::parse).collect();
 
     let mut best_score = 0;
-    for i in trees.iter() {
-        for j in trees.iter() {
-            let mut a = i.clone();
-            let mut b = j.clone();
-            add_array_tree(&mut a, &mut b);
-            reduce(&mut a);
-            best_score = best_score.max(score(&mut a));
+    for a in trees.iter() {
+        for b in trees.iter() {
+            let mut a = a.clone();
+            a.add(&b);
+            a.reduce();
+            best_score = best_score.max(a.score());
         }
     }
 
