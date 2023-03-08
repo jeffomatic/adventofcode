df1d2843edc4e5e7c9a378daa8f22cccbaa7a8c9
Jeff Lee <jeffomatic@gmail.com>
2023-03-07 20:36:59 -0800

    2015-deno/day03a: finish

---
+++ 2015-deno/day03a/main.ts
@@ -0,0 +1,28 @@
+import { readInput } from "../common.ts";
+
+import * as vec2 from "../vec2.ts";
+
+const input = readInput(import.meta.url);
+
+let pos = vec2.make(0, 0);
+const visited = new Set<string>();
+visited.add(vec2.key(pos));
+for (const c of input) {
+  switch (c) {
+    case "<":
+      vec2.add(pos, vec2.make(-1, 0), pos);
+      break;
+    case ">":
+      vec2.add(pos, vec2.make(+1, 0), pos);
+      break;
+    case "^":
+      vec2.add(pos, vec2.make(0, -1), pos);
+      break;
+    case "v":
+      vec2.add(pos, vec2.make(0, +1), pos);
+      break;
+  }
+  visited.add(vec2.key(pos));
+}
+
+console.log(visited.size);
---
+++ 2015-deno/vec2.ts
@@ -0,0 +1,75 @@
+import { signWithZero } from "./common.ts";
+
+export type Vec2 = [number, number];
+
+export function make(x: number, y: number): Vec2 {
+  return [x, y];
+}
+
+export function copy(src: Vec2, dst: Vec2 | undefined = undefined): Vec2 {
+  if (dst == undefined) {
+    dst = [0, 0];
+  }
+
+  dst[0] = src[0];
+  dst[1] = src[1];
+
+  return dst;
+}
+
+// Generates a key usable for Record<string, T> or Set<string>
+export function key(v: Vec2): string {
+  return `${v[0]}.${v[1]}`;
+}
+
+export function parseKeyInt(s: string): Vec2 {
+  const [a, b] = s.split(".");
+  return [parseInt(a), parseInt(b)];
+}
+
+export function manhattan(v: Vec2): number {
+  return Math.abs(v[0]) + Math.abs(v[1]);
+}
+
+export function add(a: Vec2, b: Vec2, out: Vec2 | undefined = undefined): Vec2 {
+  if (out == undefined) {
+    out = [0, 0];
+  }
+
+  out[0] = a[0] + b[0];
+  out[1] = a[1] + b[1];
+  return out;
+}
+
+export function sub(a: Vec2, b: Vec2, out: Vec2 | undefined = undefined): Vec2 {
+  if (out == undefined) {
+    out = [0, 0];
+  }
+
+  out[0] = a[0] - b[0];
+  out[1] = a[1] - b[1];
+  return out;
+}
+
+export function equal(a: Vec2, b: Vec2): boolean {
+  return a[0] == b[0] && a[1] == b[1];
+}
+
+export function* march(from: Vec2, to: Vec2): Generator<Vec2> {
+  const delta = sub(to, from);
+  if (delta[0] != 0 && delta[1] != 0) {
+    throw new Error(`points are diagonal`);
+  }
+
+  const step = make(signWithZero(delta[0]), signWithZero(delta[1]));
+  let cur = copy(from);
+  while (true) {
+    yield cur;
+
+    if (equal(cur, to)) {
+      break;
+    }
+
+    cur = add(cur, step);
+  }
+}
