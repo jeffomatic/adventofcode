73d175cbdb507dcaa17934655b59b34440df1f3e
Jeff Lee <jeffomatic@gmail.com>
2025-12-15 22:34:47 -0800

    2025-node/day09b: optimize

--- 2025-node/day09b/main.ts
+++ 2025-node/day09b/main.ts
@@ -68,8 +68,113 @@ function interiorDir(disp: [number, number], winding: Winding): -1 | 1 {
   throw new Error('zero displacement');
 }
 
+function overlap(a: [number, number], b: [number, number]): boolean {
+  return a[0] <= b[1] && b[0] <= a[1];
+}
+
+function flip(dir: 1 | -1): 1 | -1 {
+  return dir == 1 ? -1 : 1;
+}
+
+// Returns the subset of border spans that contain the given value along the primary axis. This will
+// only contain "exterior" borders, as determined by the given cross-axis position and exterior
+// direction. The resulting subset is sorted by ascending distance from the cross-axis position; the
+// first result is the closest border to the edge on which the given value lies.
+function getOverlappingExteriorBorders(
+  borders: PolygonSpan[],
+  edge: RectEdge,
+  dir: 1 | -1,
+): PolygonSpan[] {
+  return borders.filter((border) => {
+    if (!overlap([edge.min, edge.max], [border.min, border.max])) {
+      return false;
+    }
+
+    return dir === 1
+      ? border.crossAxisPos >= edge.crossAxisPos
+      : edge.crossAxisPos >= border.crossAxisPos;
+  });
+}
+
+// Split all provided spans so that overlapping subspans are all the same size
+function splitSpans(spans: PolygonSpan[]): PolygonSpan[] {
+  const splitPoints = new Set<number>();
+  for (const span of spans) {
+    splitPoints.add(span.min);
+    splitPoints.add(span.max);
+  }
+  const sorted = [...splitPoints].sort((a, b) => a - b);
+  return spans.flatMap((span) => {
+    const subspans: PolygonSpan[] = [];
+
+    // split the span into multiple spans along the split points
+    let start = span.min;
+    while (true) {
+      const end = sorted.find((p) => start < p && p <= span.max);
+      if (end == undefined) {
+        break;
+      }
+
+      subspans.push({ ...span, min: start, max: end });
+      start = end;
+    }
+
+    if (subspans.length === 0) {
+      subspans.push({ ...span });
+    }
+
+    return subspans;
+  });
+}
+
+// Assumes `borders` has been processed by `splitSpans()` and sorted by nearest cross-axis position,
+// making it easy for us to discover the nearest border span to the provided point.
+function checkEdgePoint(
+  mainAxisPos: number,
+  crossAxisPos: number,
+  borders: PolygonSpan[],
+  wantDir: 1 | -1,
+): number | undefined {
+  const span = borders.find((border) => border.min <= mainAxisPos && mainAxisPos <= border.max);
+  if (span === undefined) {
+    return undefined;
+  }
+
+  // Special case: the point we're checking intersects the border, which is always on "interior"
+  // regardless of the border's interior direction
+  if (crossAxisPos === span.crossAxisPos) {
+    return span.max;
+  }
+
+  return span.interiorDir == wantDir ? span.max : undefined;
+}
+
+// Determine if an edge is within the polygon. We can use the set of polygon borders that are
+// parallel to the edge.
+function checkEdge(edge: RectEdge, borders: PolygonSpan[], dir: 1 | -1): boolean {
+  // Optimization: we only need to check one point on the nearest span for each chunk of the edge.
+  // The exterior borders relative to the provided edge may overlap arbitrarily, so split them into
+  // subspans so all overlapping subspans are the same size. This lets us easily find the closest
+  // border subspan to the exterior of any point on the edge.
+  const overlappingSpans = splitSpans(getOverlappingExteriorBorders(borders, edge, dir)).sort(
+    (a, b) => dir * (a.crossAxisPos - b.crossAxisPos),
+  );
+
+  let i = edge.min;
+  while (i <= edge.max) {
+    const next = checkEdgePoint(i, edge.crossAxisPos, overlappingSpans, flip(dir));
+    if (!next) {
+      return false;
+    }
+
+    i = next + 1;
+  }
+
+  return true;
+}
+
 // const lines = example(); // 24
-const lines = input();
+const lines = input(); // 1568849600
 
 const points: [number, number][] = lines.map(
   (line) => line.split(',').map((n) => parseInt(n)) as [number, number],
@@ -99,73 +204,6 @@ for (let i = 0; i < points.length; i++) {
       interiorDir: interiorDir(disp, winding),
     });
   }
-}
-
-function overlap(a: [number, number], b: [number, number]): boolean {
-  return a[0] <= b[1] && b[0] <= a[1];
-}
-
-function flip(dir: 1 | -1): 1 | -1 {
-  return dir == 1 ? -1 : 1;
-}
-
-// Returns the subset of border spans that contain the given value along the primary axis. This will
-// only contain "exterior" borders, as determined by the given cross-axis position and exterior
-// direction. The resulting subset is sorted by ascending distance from the cross-axis position; the
-// first result is the closest border to the edge on which the given value lies.
-function getOverlappingExteriorBorders(
-  borders: PolygonSpan[],
-  edge: RectEdge,
-  dir: 1 | -1,
-): PolygonSpan[] {
-  return borders
-    .filter((border) => {
-      if (!overlap([edge.min, edge.max], [border.min, border.max])) {
-        return false;
-      }
-
-      return dir === 1
-        ? border.crossAxisPos >= edge.crossAxisPos
-        : edge.crossAxisPos >= border.crossAxisPos;
-    })
-    .sort((a, b) => dir * (a.crossAxisPos - b.crossAxisPos));
-}
-
-function checkEdgePoint(
-  mainAxisPos: number,
-  crossAxisPos: number,
-  borders: PolygonSpan[],
-  wantDir: 1 | -1,
-): boolean {
-  const span = borders.find((border) => border.min <= mainAxisPos && mainAxisPos <= border.max);
-  if (span === undefined) {
-    return false;
-  }
-
-  // Special case: the point we're checking intersects the border, which is always on "interior"
-  // regardless of the border's interior direction
-  if (crossAxisPos === span.crossAxisPos) {
-    return true;
-  }
-
-  return span.interiorDir == wantDir;
-}
-
-// Determine if an edge is within the polygon. We can use the set of polygon borders that are
-// parallel to the edge.
-function checkEdge(edge: RectEdge, borders: PolygonSpan[], dir: 1 | -1): boolean {
-  const overlappingSpans = getOverlappingExteriorBorders(borders, edge, dir);
-
-  // To optimize: we only need to check one point on the nearest span for each chunk of the edge.
-  // However, doing that requires segmenting the overlapping borders against each other, which
-  // is super annoying to do.
-  for (let i = edge.min; i <= edge.max; i++) {
-    if (!checkEdgePoint(i, edge.crossAxisPos, overlappingSpans, flip(dir))) {
-      return false;
-    }
-  }
-
-  return true;
 }
 
 let best = -1;
