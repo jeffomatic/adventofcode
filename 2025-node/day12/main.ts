10138bccf5e6cf62f09b74afcc37384a25312849
Jeff Lee <jeffomatic@gmail.com>
2025-12-26 12:14:16 -0800

    2025-node/day12: finish

--- 2025-node/day12/main.ts
+++ 2025-node/day12/main.ts
@@ -1,16 +1,18 @@
 import { example, input } from '../lib/util';
 
 type Shape = boolean[][];
+
 type Region = {
   width: number;
   height: number;
   presents: number[];
 };
+
 const shapes: Shape[] = [];
 const regions: Region[] = [];
 
-const lines = example(); // 2
-// const lines = input();
+// const lines = example(); // 2
+const lines = input();
 
 let parsingShapes = true;
 for (let i = 0; i < lines.length; i++) {
@@ -40,168 +42,37 @@ for (let i = 0; i < lines.length; i++) {
   }
 }
 
-function serializeCoord(coord: [number, number]): string {
-  return `${coord[0]},${coord[1]}`;
-}
-
-function deserializeCoord(s: string): [number, number] {
-  return s.split(',').map(Number) as [number, number];
-}
-
-function compareCoord(a: [number, number], b: [number, number]): number {
-  if (a[0] - b[0] < 0) {
-    return -1;
-  }
-
-  if (b[0] - a[0] < 0) {
-    return 1;
-  }
-
-  if (a[1] - b[1] < 0) {
-    return -1;
-  }
-
-  if (b[1] - a[1] < 0) {
-    return 1;
-  }
-
-  return 0;
-}
-
-function serializeRegion(r: Set<string>): string {
-  return [...r].map(deserializeCoord).sort(compareCoord).map(serializeCoord).join('|');
-}
-
-function printRegion(r: Set<string>, width: number, height: number) {
-  for (let i = 0; i < height; i++) {
-    let rowStr = '';
-    for (let j = 0; j < width; j++) {
-      if (r.has(serializeCoord([j, i]))) {
-        rowStr += '#';
-      } else {
-        rowStr += '.';
-      }
+function shapeWeight(shape: Shape) {
+  let weight = 0;
+  for (const row of shape) {
+    for (const cell of row) {
+      weight += cell ? 1 : 0;
     }
-    console.log(rowStr);
   }
-  console.log();
+  return weight;
 }
 
-function rotate(present: Shape, rot: number): Shape {
-  let result = structuredClone(present);
+let naiveFits = 0;
+let exactFits = 0;
+for (const r of regions) {
+  const area = r.width * r.height;
+  const naive = r.presents.reduce((accum, count) => accum + 9 * count, 0);
+  const exact = r.presents.reduce(
+    (accum, count, index) => accum + shapeWeight(shapes[index]) * count,
+    0,
+  );
 
-  for (let i = 0; i < rot; i++) {
-    result = [
-      [result[2][0], result[1][0], result[0][0]],
-      [result[2][1], result[1][1], result[0][1]],
-      [result[2][2], result[1][2], result[0][2]],
-    ];
+  if (naive <= area) {
+    naiveFits += 1;
   }
 
-  return result;
+  if (exact <= area) {
+    exactFits += 1;
+  }
+
+  console.log(
+    `${r.width}x${r.height}: area: ${area}, naive ${naive}${naive <= area ? ' (fits)' : ''}, exact ${exact}${exact <= area ? ' (fits)' : ''}`,
+  );
 }
 
-function printShape(shape: Shape) {
-  for (let i = 0; i < 3; i++) {
-    let rowStr = '';
-    for (let j = 0; j < 3; j++) {
-      if (shape[i][j]) {
-        rowStr += '#';
-      } else {
-        rowStr += '.';
-      }
-    }
-    console.log(rowStr);
-  }
-  console.log();
-}
-
-class Solver {
-  visited = new Set<string>();
-
-  constructor(
-    public width: number,
-    public height: number,
-  ) {}
-
-  attemptPlace(
-    present: Shape,
-    x: number,
-    y: number,
-    rot: number,
-    region: Set<string>,
-  ): Set<string> | undefined {
-    const rotated = rotate(present, rot);
-    const newCoords: string[] = [];
-
-    for (let i = 0; i < 3; i++) {
-      const row = rotated[i];
-      for (let j = 0; j < 3; j++) {
-        if (!row[j]) {
-          continue;
-        }
-
-        const coordStr = serializeCoord([x + j, y + i]);
-        if (region.has(coordStr)) {
-          return undefined;
-        }
-
-        newCoords.push(coordStr);
-      }
-    }
-
-    const res = new Set(region);
-    for (const coord of newCoords) {
-      res.add(coord);
-    }
-
-    return res;
-  }
-
-  solve(presents: number[], occupied: Set<string>): boolean {
-    // console.log(presents);
-
-    if (presents.every((p) => p === 0)) {
-      return true;
-    }
-
-    // TODO: visited probably needs to include `presents`
-    const visitedKey = presents.join(',') + ':' + serializeRegion(occupied);
-    if (this.visited.has(visitedKey)) {
-      // console.log('dedupe');
-      return false;
-    }
-
-    this.visited.add(visitedKey);
-
-    const newPresents = [...presents];
-    const presentIndex = newPresents.findIndex((n) => n > 0);
-    newPresents[presentIndex] -= 1;
-    const shape = shapes[presentIndex];
-
-    for (let y = 0; y < this.height - 2; y++) {
-      for (let x = 0; x < this.width - 2; x++) {
-        for (let rot = 0; rot < 4; rot++) {
-          const nextOccupied = this.attemptPlace(shape, x, y, rot, occupied);
-          if (nextOccupied !== undefined) {
-            if (this.solve(newPresents, nextOccupied)) {
-              return true;
-            }
-          }
-        }
-      }
-    }
-
-    return false;
-  }
-}
-
-let res = 0;
-for (const region of regions) {
-  const solver = new Solver(region.width, region.height);
-  if (solver.solve(region.presents, new Set())) {
-    res += 1;
-  }
-}
-
-console.log(res);
+console.log(`naive fits: ${naiveFits}, exactFits: ${exactFits}`);
